6a2d3b89afa64626fb6e7fcaad5ca0420fa4a0c3
aswinak77 <aswinak770@gmail.com>
2026-03-03 12:42:12 +0530

    removing build and devel

--- build/global_planner_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aswin/project_ws/src/global_planner_sim
-# Build directory: /home/aswin/project_ws/build/global_planner_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
